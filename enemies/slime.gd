085acf409082c2a3e1beb532d502c79723c82e63
Abel Goy <agoy@ucsc.edu>
2024-01-14 22:15:49 -0800

    health properly subtracting

--- enemies/slime.gd
+++ enemies/slime.gd
@@ -47,6 +47,9 @@ func _physics_process(delta):
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
+	player.currentHealth -= 1
+	print_debug("Health: ", player.currentHealth)
+	
 	
 func _on_detection_area_body_exited(body):
 	player = null

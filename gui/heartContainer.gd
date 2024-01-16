6e50fae3006a8ec1ff4c38d372ca07b3c90bd41d
Abel Goy <agoy@ucsc.edu>
2024-01-15 21:03:22 -0800

    max hearts settable

---
+++ gui/heartContainer.gd
@@ -0,0 +1,17 @@
+extends HBoxContainer
+
+@onready var HeartGuiClass = preload("res://gui/heart.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func setMaxHearts(max: int):
+	for i in range(max):
+		var heart = HeartGuiClass.instantiate()
+		add_child(heart)
